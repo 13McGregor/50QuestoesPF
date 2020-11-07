3d3382d80c38e32272566a759c2e01dc29b83fc4
13McGregor <miguel.rocha.0603@gmail.com>
2020-11-07 02:01:17 +0000

    Questões 14...20

--- 50q.hs
+++ 50q.hs
@@ -79,4 +79,56 @@ myConcat :: [[a]] -> [a]
 myConcat [] = []
 myConcat (h:t) = h ++ (myConcat t)
 
---test vscode
+--14
+myInits :: [a] -> [[a]]
+myInits [] = [[]]
+myInits l = myInits (myInit l) ++ [l]
+
+myInit :: [a] -> [a]
+myInit [a] = []
+myInit (h:t) = h : myInit (t)
+
+--15
+myTails :: [a] -> [[a]]
+myTails [] = [[]]
+myTails (h:t) = [(h:t)] ++ myTails (t)
+
+--16
+myIsPrefixOf :: Eq a => [a] -> [a] -> Bool
+myIsPrefixOf [] _ = True
+myIsPrefixOf (h1:t1) (h2:t2) | h1 /= h2 = False
+                             | h1 == h2 = myIsPrefixOf t1 t2
+
+--17                             
+myIsSuffixOf :: Eq a => [a] -> [a] -> Bool
+myIsSuffixOf [] _ = True
+myIsSuffixOf l1 l2 | last l1 /= last l2 = False
+                   | last l1 == last l2 = myIsSuffixOf (myInit l1) (myInit l2)
+
+--18
+myIsSubsequenceOf :: Eq a => [a] -> [a] -> Bool
+myIsSubsequenceOf [] _ = True
+myIsSubsequenceOf _ [] = False
+myIsSubsequenceOf (h1:t1) (h2:t2) | h1 == h2 = myIsSubsequenceOf t1 t2
+                                  | h1 /= h2 = myIsSubsequenceOf (h1:t1) t2
+
+--19
+myElemIndices :: Eq a => a -> [a] -> [Int]
+myElemIndices a [] = []
+myElemIndices a l = aux19 0 a l
+
+aux19 :: Eq a => Int -> a -> [a] -> [Int]
+aux19 _ _ [] = []
+aux19 n i (h:t) | i == h = n : (aux19 (n+1) i t) 
+                | i /= h = aux19 (n+1) i t 
+
+--20
+myNub :: Eq a => [a] -> [a]
+myNub [] = []
+myNub [a] = [a]
+myNub (h:t) = h : myNub (aux20 h t)
+
+aux20 :: Eq a => a -> [a] -> [a]
+aux20 a [] = []
+aux20 a (h:t) | a == h = aux20 a t
+              | otherwise = h : aux20 a t
